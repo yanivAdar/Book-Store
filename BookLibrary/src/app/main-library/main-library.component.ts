7ae7d55a1dfc701b651148e120a5116166d32fa4
yaniv adar <yanivmusic360@gmail.com>
2018-04-23 12:38:16 +0300

    bugs fix

--- BookLibrary/src/app/main-library/main-library.component.ts
+++ BookLibrary/src/app/main-library/main-library.component.ts
@@ -2,23 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MainLibraryService } from '../services/main-library.service';
 import { Popup } from 'ng2-opd-popup';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { trigger, style, animate, transition } from '@angular/animations';
 import { CostumValidatorsService } from '../services/costum-validators.service';
-
+import { Animations } from './main-library.animation';
 
 @Component({
   selector: 'app-main-library',
-  animations: [
-    trigger('fadeInOut', [
-      transition(':enter', [  
-        style({ opacity: 0 }),
-        animate(300, style({ opacity: 1 }))
-      ]),
-      transition(':leave', [  
-        animate(300, style({ opacity: 0 }))
-      ])
-    ])
-  ],
+  animations: new Animations().animations,
   templateUrl: './main-library.component.html',
   styleUrls: ['./main-library.component.css']
 })
@@ -63,7 +52,7 @@ export class MainLibraryComponent implements OnInit {
     this.createAddForm = new FormGroup({
       'Title': new FormControl(title, Validators.required),
       'Author': new FormControl(author, Validators.required),
-      'Date': new FormControl(date, Validators.compose([Validators.required,this.costumValidators.date]))
+      'Date': new FormControl(date, Validators.compose([Validators.required, this.costumValidators.date]))
     })
   }
 
@@ -81,19 +70,19 @@ export class MainLibraryComponent implements OnInit {
     this.cleanFormAndClosePopup();
   }
   onDelete() {
-    this.booksCollection.splice(this.bookId, 1);
     this.cleanFormAndClosePopup();
+    this.booksCollection.splice(this.bookId, 1);
   }
   cleanFormAndClosePopup() {
     this.popup.hide();
     this.initForm(null, null, null);
-    this.editMode = false;   
-    this.isTitleValid = false; 
+    this.editMode = false;
+    this.isTitleValid = false;
   }
-  checkValidTitle(event){
-    this.isTitleValid = false; 
+  checkValidTitle(event) {
+    this.isTitleValid = false;
     this.booksCollection.map(book => {
-      if(book['Title'] == event.target.value){
+      if (book['Title'] == event.target.value) {
         this.isTitleValid = true;
       }
     })
